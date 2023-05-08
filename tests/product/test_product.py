a2d8c56a0ba212c676e647c9da6b43abb848a0e4
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-08 14:57:34 -0300

    Req 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -4,18 +4,18 @@ from inventory_report.inventory.product import Product
 def test_cria_produto():
     test_product = Product(
         1,
-        "CADEIRA",
-        "Forces of Nature",
+        "Cult",
+        "Razors",
         "2022-04-04",
-        "2023-02-09",
-        "FR48",
-        "Sentar com cuidado."
+        "Indeterminado",
+        "321",
+        "Use sempre prteção."
     )
 
     assert test_product.id == 1
-    assert test_product.nome_do_produto == "CADEIRA"
-    assert test_product.nome_da_empresa == "Forces of Nature"
+    assert test_product.nome_do_produto == "Cult"
+    assert test_product.nome_da_empresa == "Razors"
     assert test_product.data_de_fabricacao == "2022-04-04"
-    assert test_product.data_de_validade == "2023-02-09"
-    assert test_product.numero_de_serie == "FR48"
-    assert test_product.instrucoes_de_armazenamento == "Sentar com cuidado."
+    assert test_product.data_de_validade == "Indeterminado"
+    assert test_product.numero_de_serie == "321"
+    assert test_product.instrucoes_de_armazenamento == "Use sempre prteção."
